1b62c2ccd3d463ed660c4136cbf23f61c4a796d3
Joao <joaogabriel.sc99@gmail.com>
2025-01-24 13:37:49 -0300

    fix: adding more flexibility

--- database/models.py
+++ database/models.py
@@ -1,13 +1,16 @@
 from sqlalchemy import Column, Integer, Float
 from .database import Base
 
+origem: float = 0.0
+
 class ObjectVariables(Base):
     __tablename__ = "object_variables"
+    
     id = Column(Integer, primary_key=True, index=True)
-    position_x = Column(Float, default=0.0)
-    position_y = Column(Float, default=0.0)
-    position_z = Column(Float, default=0.0)
-    width = Column(Float, default=0.0)
-    height = Column(Float, default=0.0)
-    length = Column(Float, default=0.0)
-    angle = Column(Float, default=0.0)
+    position_x = Column(Float, default=origem)
+    position_y = Column(Float, default=origem)
+    position_z = Column(Float, default=origem)
+    width = Column(Float, default=origem)
+    height = Column(Float, default=origem)
+    length = Column(Float, default=origem)
+    angle = Column(Float, default=origem)
